f0a5571baeb0651729b42a90f85838f7cd16c8fb
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-18 00:33:12 -0300

    Feat: Server setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app";
+import env from "./env";
+
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => console.log("Server running on port: " + port));
+  })
+  .catch(console.error);
