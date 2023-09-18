f945ff6ac378ca7b873a870a78c55e3f7efd20e7
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-18 13:02:58 -0300

    Feat: Set up cors and morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import blogPostRoutes from "./routes/blogPosts";
+import cors from "cors";
+import env from "./env";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: env.FRONT_URL,
+  })
+);
 
 app.use("/posts", blogPostRoutes);
 
